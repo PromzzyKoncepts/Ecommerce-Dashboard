3a2e656ff7cb27bf19be67c7d1214bb88205ceb0
adigwechizoba181@gmail.com <adigwechizoba181@gmail.com>
2023-11-17 13:53:31 +0000

    dev setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home(params) {
+    return(
+        <>
+        <div className="bg-black text-cyan-400 text-lg">
+            WELCOME MY FRONTEND CHILDREN
+        </div>
+        </>
+    )
+}
+export default Home
