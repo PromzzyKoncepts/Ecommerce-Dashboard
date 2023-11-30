53bc3e494c4ac824a84e325e78c5d42bdf705265
Promise Okechukwu <okechukwupromise638@gmail.com>
2023-11-30 12:38:06 +0100

    fix bugs again

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import ProductOverview from "./pages/ProductsOverview";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import OrderSummary from "./pages/OrderSummary";
 import MainLayout from "./components/MainLayout";
 
 function App() {
-
   return (
     <>
       <Routes>
-          <Route path="/vendor" element={<MainLayout />} >
-            <Route index element={<Dashboard />} />
-          <Route path="/order" element={<OrderSummary />} />
-          <Route path="/products" element={<ProductOverview />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="product-overview" element={<ProductOverview />} />
           <Route path="/login" element={<Login />} />
-          </Route>
-
+        </Route>
       </Routes>
     </>
   );
