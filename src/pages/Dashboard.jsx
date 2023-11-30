5e0badf1c1bb763802eb706f27d4f5e5be604946
Promise Okechukwu <okechukwupromise638@gmail.com>
2023-11-30 15:12:24 +0100

    minimal changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import { HiArrowTrendingUp, HiArrowTrendingDown } from "react-icons/hi2";
 import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
 import { SalesData } from '../data/SalesData';
 import { Table } from 'antd';
+import userContext from "../context/userContext"
 
 
 
 
 
 const Dashboard = () => {
+
+  const { authUser } = useContext(userContext)
   const data = [
     {
       name: 'Page A',
@@ -93,43 +96,62 @@ const Dashboard = () => {
 
   return (
     <div>
-      <h3 className='mb-6 font-semibold text-3xl text-slate-300'> Dashboard </h3>
-      <p className='mb-6 font-semibold text-xl'> Good Afternoon, Vendor Name </p>
-      <div className='flex justify-between items-center gap-3'>
-
-        <div className='flex justify-between items-center grow bg-white p-8 rounded'>
+      <h3 className="mb-6 font-semibold text-3xl text-slate-300">
+        {" "}
+        Dashboard{" "}
+      </h3>
+      <p className="mb-6 font-semibold text-xl">
+        {" "}
+        Good Afternoon, {authUser.business_name}{" "}
+      </p>
+      <p className="text-sm font-light">
+        Logged in as: {authUser.first_name} {authUser.last_name}
+      </p>
+      <div className="flex justify-between items-center gap-3">
+        <div className="flex justify-between items-center grow bg-white p-8 rounded">
           <div>
-            <p className='mb-2'>Total</p>
-            <h4 className='mb-0 text-xl font-medium'>₦478,020</h4>
+            <p className="mb-2">Total</p>
+            <h4 className="mb-0 text-xl font-medium">₦478,020</h4>
           </div>
-          <div className='flex flex-col items-end'>
-            <h6 className='flex mb-2'><HiArrowTrendingUp className='text-green-500 pt-1 pr-1' />72%</h6>
-            <h4 className='mb-0'>Compared to August 2023</h4>
+          <div className="flex flex-col items-end">
+            <h6 className="flex mb-2">
+              <HiArrowTrendingUp className="text-green-500 pt-1 pr-1" />
+              72%
+            </h6>
+            <h4 className="mb-0">Compared to August 2023</h4>
           </div>
         </div>
-        <div className='flex justify-between items-center grow bg-white p-8 rounded'>
+        <div className="flex justify-between items-center grow bg-white p-8 rounded">
           <div>
-            <p className='mb-2'>Total</p>
-            <h4 className='mb-0 text-xl font-medium'>₦478,020</h4>
+            <p className="mb-2">Total</p>
+            <h4 className="mb-0 text-xl font-medium">₦478,020</h4>
           </div>
-          <div className='flex flex-col items-end'>
-            <h6 className='flex mb-2'><HiArrowTrendingDown className='text-red-500 pt-1 pr-1' />41%</h6>
-            <h4 className='mb-0'>Compared to Semptember 2023</h4>
+          <div className="flex flex-col items-end">
+            <h6 className="flex mb-2">
+              <HiArrowTrendingDown className="text-red-500 pt-1 pr-1" />
+              41%
+            </h6>
+            <h4 className="mb-0">Compared to Semptember 2023</h4>
           </div>
         </div>
-        <div className='flex justify-between items-center grow bg-white p-8 rounded'>
+        <div className="flex justify-between items-center grow bg-white p-8 rounded">
           <div>
-            <p className='mb-2'>Total</p>
-            <h4 className='mb-0 text-xl font-medium'>₦478,020</h4>
+            <p className="mb-2">Total</p>
+            <h4 className="mb-0 text-xl font-medium">₦478,020</h4>
           </div>
-          <div className='flex flex-col items-end'>
-            <h6 className='flex mb-2'><HiArrowTrendingUp className='text-green-500 pt-1 pr-1' />65%</h6>
-            <h4 className='mb-0'>Compared to October 2023</h4>
+          <div className="flex flex-col items-end">
+            <h6 className="flex mb-2">
+              <HiArrowTrendingUp className="text-green-500 pt-1 pr-1" />
+              65%
+            </h6>
+            <h4 className="mb-0">Compared to October 2023</h4>
           </div>
         </div>
       </div>
-      <div className='mt-4'>
-        <h3 className='mb-8 font-semibold text-2xl mt-10 mr-10'>Income Statics</h3>
+      <div className="mt-4">
+        <h3 className="mb-8 font-semibold text-2xl mt-10 mr-10">
+          Income Statics
+        </h3>
         <div className="container mx-auto p-4">
           <BarChart width={1100} height={300} data={SalesData} className="  ">
             <XAxis dataKey="month" stroke="#C89E0E" />
@@ -140,22 +162,19 @@ const Dashboard = () => {
           </BarChart>
         </div>
       </div>
-      <div className='mt-4'>
-        <h3 className='mb-8 font-semibold text-2xl mt-10'>Recent Orders</h3>
+      <div className="mt-4">
+        <h3 className="mb-8 font-semibold text-2xl mt-10">Recent Orders</h3>
         <div>
           <Table columns={columns} dataSource={data2} pagination={pagination} />
         </div>
       </div>
 
-      <div className='mt-4'>
-        <h3 className='mb-8 font-semibold text-2xl mt-10'>Recent Reviews</h3>
-        <div>
-        </div>
+      <div className="mt-4">
+        <h3 className="mb-8 font-semibold text-2xl mt-10">Recent Reviews</h3>
+        <div></div>
       </div>
-
-
     </div>
-  )
+  );
 }
 
 export default Dashboard
